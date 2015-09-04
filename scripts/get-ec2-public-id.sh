0d5f45344e07c95b3ce86986c6bf82e6a15055dc
Xu Wang <xuwang@gmail.com>
2015-09-04 04:44:37 +0000

    use the default the profile default region

--- scripts/get-ec2-public-id.sh
+++ scripts/get-ec2-public-id.sh
@@ -1,12 +1,11 @@
 #!/usr/bin/env bash
 
-AWS_REGION=${2:-us-west-2}
-AWS_PROFILE=${3:-mycluster}
+AWS_PROFILE=${AWS_PROFILE:-mycluster}
 ASG=${1:-etcd}
 
 EC2_IDS=$(aws --profile $AWS_PROFILE autoscaling describe-auto-scaling-groups \
-        --region $AWS_REGION --auto-scaling-group-name $ASG \
+     --auto-scaling-group-name $ASG \
     | jq .AutoScalingGroups[0].Instances[].InstanceId | xargs)
 
-aws --profile $AWS_PROFILE ec2 describe-instances --region $AWS_REGION --instance-ids $EC2_IDS \
+aws --profile $AWS_PROFILE ec2 describe-instances --instance-ids $EC2_IDS \
     | jq -r '.Reservations[].Instances | map(.NetworkInterfaces[].Association.PublicIp)[]'
